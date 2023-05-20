3f0fdbb956140e97586f67d31c96ee4fe324c135
louiseameliemarechal <louiseamelie2@gmail.com>
2023-05-20 18:54:35 +0200

    added corresponding links to the footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,8 +4,8 @@ const Footer = () => {
 
     return (
         <div className="footer">
-            <button className={"fa-brands fa-linkedin fa-xl"} style={{color: "var(--pink)"}}></button>
-            <button className={"fa-brands fa-square-github fa-xl"} style={{color: "var(--pink)"}}></button>
+            <a href='https://www.linkedin.com/in/louise-amelie-mar%C3%A9chal-ba978298'><i className={"fa-brands fa-linkedin fa-xl"} style={{color: "var(--pink)"}}></i></a>
+            <a href='https://github.com/louiseamarechal'><i className={"fa-brands fa-square-github fa-xl"} style={{color: "var(--pink)"}}></i></a>
         </div>
     )
 }
